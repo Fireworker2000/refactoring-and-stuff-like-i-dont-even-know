9916c4600b23d497535a22059710a5cc2221f01a
Fireworker2000 <52700461+Fireworker2000@users.noreply.github.com>
2020-05-17 00:34:14 +0200

    Remade linebreaks in code

--- src/Rental.java
+++ src/Rental.java
@@ -1,10 +1,12 @@
 class Rental {
     private Movie movie;
     private int daysRented;
+
     public Rental(Movie newmovie, int newdaysRented) {
         movie = newmovie;
         daysRented = newdaysRented;
     }
+
     public int getDaysRented() {
         return daysRented;
     }
