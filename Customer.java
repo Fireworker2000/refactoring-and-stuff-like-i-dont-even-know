1ece1aa646f2e04c7424a5cb65dfeb10aec2a290
Fireworker2000 <52700461+Fireworker2000@users.noreply.github.com>
2020-05-17 01:07:01 +0200

    Fixed result building in customer class
    Swapped String variables for Stringbuilder ones, removed unneccessary String.valueOf()-calls

---
+++ Customer.java
@@ -0,0 +1,72 @@
+
+import java.lang.*;
+import java.util.*;
+
+class Customer {
+    private final String name;
+    private final Vector rentals;
+
+    public Customer (String newname){
+        name = newname;
+        rentals = new Vector();
+    }
+
+    public void addRental(Rental arg) { rentals.addElement(arg); }
+
+    public String getName (){
+        return name;
+    };
+
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        Enumeration enum_rentals = rentals.elements();
+
+        StringBuilder result = new StringBuilder("Rental Record for " + this.getName() + "\n");
+        result.append("\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n");
+
+        while (enum_rentals.hasMoreElements()) {
+            Rental rental = (Rental) enum_rentals.nextElement();
+
+            double thisAmount = amountFor(rental); // determine amounts for each line
+            frequentRenterPoints ++;      // add frequent renter points
+
+            // add bonus for a two day new release rental
+            if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1)
+                frequentRenterPoints ++;
+
+            //show figures for this rental
+            result.append("\t").append(rental.getMovie().getTitle()).append("\t").append("\t").append(rental.getDaysRented()).append("\t").append(thisAmount).append("\n");
+            totalAmount += thisAmount;
+        }
+
+        //add footer lines
+        result.append("Amount owed is ").append(totalAmount).append(".\n");
+        result.append("You earned ").append(frequentRenterPoints).append(" totally worthless loyalty points.");
+        return result.toString();
+    }
+
+    private double amountFor(Rental rental) {
+        double thisAmount = 0;
+        switch (rental.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (rental.getDaysRented() > 2)
+                    thisAmount += (rental.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += rental.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (rental.getDaysRented() > 3)
+                    thisAmount += (rental.getDaysRented() - 3) * 1.5;
+                break;
+            default:
+                break;
+        }
+        return thisAmount;
+    }
+
+}
+    
